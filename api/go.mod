16fe34391acfb4b276e7842a65705a90e8533dc2
Vatsal <chauhanvatsal55@gmail.com>
2022-03-22 13:54:00 +0530

    feat: added fiber google auth #6

--- api/go.mod
+++ api/go.mod
@@ -2,11 +2,31 @@ module googleauth
 
 go 1.17
 
-require golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+require (
+	github.com/gofiber/fiber/v2 v2.30.0
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/joho/godotenv v1.4.0
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
 
 require (
-	github.com/golang/protobuf v1.2.0 // indirect
-	github.com/joho/godotenv v1.4.0
-	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
-	google.golang.org/appengine v1.4.0 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/markbates/goth v1.69.0
+	github.com/shareed2k/goth_fiber v0.2.6
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.34.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+)
+
+require (
+	cloud.google.com/go v0.67.0 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.1.1 // indirect
+	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
--- api/main.go
+++ api/main.go
@@ -1,37 +1,21 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	"golang.org/x/oauth2"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/providers/google"
+	gf "github.com/shareed2k/goth_fiber"
 )
 
-var (
-	config = oauth2.Config{
-		ClientID:     "152904262856-jab3ep02vvgajdc66ab7ehafenoluln4.apps.googleusercontent.com",
-		ClientSecret: "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",
-		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-		RedirectURL:  "http://localhost:3000/auth/google/callback",
-		// This points to our Authorization Server
-		// if our Client ID and Client Secret are valid
-		// it will attempt to authorize our user
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
-			TokenURL: "https://oauth2.googleapis.com/token",
-		},
-	}
+const (
+	key = "152904262856-jab3ep02vvgajdc66ab7ehafenoluln4.apps.googleusercontent.com"
+	sec = "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"
 )
 
-var (
-	randState = fmt.Sprintf("st%d", time.Now().UnixNano())
-)
 // LoadENV
 func ConnectENV() {
 	err := godotenv.Load(".env")
@@ -39,53 +23,44 @@ func ConnectENV() {
 		log.Fatal(".env file not loaded properly")
 	}
 }
-// Homepage
-func HomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Homepage Hit!")
-	u := config.AuthCodeURL(randState)
-	http.Redirect(w, r, u, http.StatusFound)
-}
-
-// Authorize
-func Authorize(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	state := r.Form.Get("state")
-	if state != randState {
-		http.Error(w, "State invalid", http.StatusBadRequest)
-		return
-	}
-
-	code := r.Form.Get("code")
-	if code == "" {
-		http.Error(w, "Code not found", http.StatusBadRequest)
-		return
-	}
-
-	token, err := config.Exchange(context.Background(), code)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	e := json.NewEncoder(w)
-	e.SetIndent("", "  ")
-	e.Encode(*token)
-}
 
 func main() {
 	ConnectENV()
-	// 1 - We attempt to hit our Homepage route
-	// if we attempt to hit this unauthenticated, it
-	// will automatically redirect to our Auth
-	// server and prompt for login credentials
-	http.HandleFunc("/", HomePage)
+	goth.UseProviders(
+		google.New(key, sec, "http://localhost:3000/auth/google/callback"),
+	)
 
-	// 2 - This displays our state, code and
-	// token and expiry time that we get back
-	// from our Authorization server
-	http.HandleFunc("/oauth2", Authorize)
+	app := fiber.New()
 
-	// 3 - We start up our Client on port 9094
-	log.Println("Client is running at 9094 port.")
-	log.Fatal(http.ListenAndServe(os.ExpandEnv(":${PORT}"), nil))
+	app.Get("/auth/:provider/callback", func(ctx *fiber.Ctx) error {
+		user, err := gf.CompleteUserAuth(ctx)
+		if err != nil {
+			return err
+		}
+		ctx.JSON(user)
+		return nil
+	})
+
+	app.Get("/logout/:provider", func(ctx *fiber.Ctx) error {
+		gf.Logout(ctx)
+		ctx.Redirect("/")
+		return nil
+	})
+
+	app.Get("/auth/:provider", func(ctx *fiber.Ctx) error {
+		if gothUser, err := gf.CompleteUserAuth(ctx); err == nil {
+			ctx.JSON(gothUser)
+		} else {
+			gf.BeginAuthHandler(ctx)
+		}
+		return nil
+	})
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		ctx.Format("<p><a href='/auth/google'>google</a></p>")
+		return nil
+	})
+
+	log.Fatal(app.Listen(os.ExpandEnv(":${PORT}")))
+
 }
