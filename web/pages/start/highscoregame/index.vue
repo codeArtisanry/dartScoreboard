ceca14e02efb1ba19054877538d24e7b37a936ed
Jeel Rupapara <48467846+zeelrupapara@users.noreply.github.com>
2022-03-25 11:32:59 +0530

    feat: create UI start game page (#26)
    * feat: create UI start game page
    
    * fix: code with lintfix
    
    * fix: change import root with use of @

---
+++ web/pages/start/highscoregame/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Startgame :game-type="gameType" />
+</template>
+<script>
+import Startgame from '@/components/Startgame.vue'
+export default {
+  components: { Startgame },
+  data() {
+    return {
+      gameType: {
+        type: 'High Score Game',
+        score: 'Score',
+      },
+    }
+  },
+}
+</script>
