2a8eba3f99fc07f817eaa1688e1861b652c80baf
Vatsal <chauhanvatsal55@gmail.com>
2022-03-29 12:51:38 +0530

    fix: token

--- api/main.go
+++ api/main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"time"
 
-	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"github.com/markbates/goth"
@@ -45,17 +44,17 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		// TODO: Check from cookie if user is exist or not
 		// if exist then return 2xx status code
-		if c.Cookies("userinfo") == c.Cookies("frontend") {
-			fmt.Println(c.Cookies("userinfo"))
-			return c.JSON(fiber.Map{"status": "success", "message": "Success login"})
-			// Else return 403 unauthorized
-		} else {
-			return c.Redirect("/auth/google")
-		}
+		// if c.Cookies("userinfo") == c.Cookies("frontend") {
+		// 	fmt.Println(c.Cookies("userinfo"))
+		// 	return c.JSON(fiber.Map{"status": "success", "message": "Success login"})
+		// 	// Else return 403 unauthorized
+		// } else {
+		// }
 
 		// If exist then return 2xx status code
 		// return nil
-		// return c.JSON(fiber.Map{"status": "Fail", "message": "unauthorized user"})
+		c.Redirect("/auth/google")
+		return c.JSON(fiber.Map{"status": "Fail", "message": "unauthorized user"})
 	})
 
 	// 2. Initiate google signin flow
@@ -74,6 +73,7 @@ func main() {
 		if err != nil {
 			return err
 		}
+		fmt.Println(userinfo.IDToken)
 		user := models.User{
 			Id:      userinfo.UserID,
 			Email:   userinfo.Email,
@@ -83,40 +83,29 @@ func main() {
 		fmt.Println("from api", user)
 		models.InsertData(db, user)
 
-		if user.Id == "" {
-			ctx.Status(fiber.StatusNotFound)
-			return ctx.JSON(fiber.Map{
-				"message": "user not found",
-			})
-		}
+		// if user.Id == "" {
+		// 	ctx.Status(fiber.StatusNotFound)
+		// 	return ctx.JSON(fiber.Map{
+		// 		"message": "user not found",
+		// 	})
+		// }
 		fmt.Println(userinfo)
 		// GET TOKEN
-		// token := ctx.Cookies("userinfo")
-		token := jwt.New(jwt.SigningMethodHS256)
-
-		claims := token.Claims.(jwt.MapClaims)
-		claims["identity"] = userinfo
-		claims["admin"] = true
-		claims["exp"] = time.Now().Add(24 * time.Hour * 72).Unix()
-
-		t, err := token.SignedString([]byte(`eyJhbGciOiJSUzI1NiIsImtpZCI6IjU4YjQyOTY2MmRiMDc4NmYyZWZlZmUxM2MxZWIxMmEyOGRjNDQyZDAiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiIxNTI5MDQyNjI4NTYtamFiM2VwMDJ2dmdhamRjNjZhYjdlaGFmZW5vbHVsbjQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiIxNTI5MDQyNjI4NTYtamFiM2VwMDJ2dmdhamRjNjZhYjdlaGFmZW5vbHVsbjQuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMTM1MzEyMjk1MzA4MTgyNjU4ODMiLCJoZCI6ImltcHJvd2lzZWQuY29tIiwiZW1haWwiOiJ2YXRzYWxAaW1wcm93aXNlZC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IlNvUGV4aGdoRlJ2bXNQN3FPNWZ5V0EiLCJpYXQiOjE2NDg1MzAwMTUsImV4cCI6MTY0ODUzMzYxNX0.MzHc_mGAF3tdeeklVWVlbV_8KiInfXcv_cVvz9P-2CJIEHW2HA63eO1W3VxT22sXGZkegvhFYdpMyYo_L8fugSX-4TbaRUjSOPzmQXAgoiXMfedHNGejQH50ciGxp4KZVo3P8sC4vZJglzyEh8pFOlISFjAQiIh3vWkLDZTcmAuEmzo5KlM4O88e5k7F47dS_qArqpPNRpvr1gOddf5HvYYYkRVLem2njLxc9qe7oqX80GGZW32zkUoPe4467TRsh0T9uihPj6ue8SBAN60vnEqtnrZ7c_YPjNGXJaPG3HfilxraBtWNp1Jl_8t4vSLtq9wbKHISLibXGaXLs8ykFQ`))
-		fmt.Println("token by google: ", t)
-		if err != nil {
-			return ctx.SendStatus(fiber.StatusInternalServerError)
-		}
+		fmt.Println("ID TOKEN", userinfo.IDToken)
 
 		// TODO: Set cookie
 		cookie := new(fiber.Cookie)
 		cookie.Name = "userinfo"
-		cookie.Value = t
+		cookie.Value = userinfo.IDToken
 		cookie.Expires = time.Now().Add(30 * time.Hour * 24)
+		cookie.HTTPOnly = true
 		// Set cookie from JWT
 		ctx.Cookie(cookie)
 		// TODO: Redirect user to frontend
 		ctx.Redirect("/home")
 		return ctx.JSON(fiber.Map{
 			"message": "success",
-			"data":    t,
+			"data":    userinfo,
 		})
 	})
 
