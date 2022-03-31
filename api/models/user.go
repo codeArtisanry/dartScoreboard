dcfad8a08ec4c6b95ba031de385bb29838562849
zeelrupapara <zeelrupapara@gmail.com>
2022-03-31 12:46:07 +0530

    fix: update struct releted to db

--- api/models/user.go
+++ api/models/user.go
@@ -1,25 +1,38 @@
 package models
 
+import "time"
+
 type User struct {
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
-	Email   string `json:"email"`
-	AvatarURL string `json:"avatarUrl"`
+	Id        int        `json:"id"`
+	FirstName string     `json:"firstName"`
+	LastName  string     `json:"lastName"`
+	Email     string     `json:"email"`
+	AvatarURL string     `json:"avatarUrl"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 }
 
 type Game struct {
-	Name        string `json:"gameName"`
-	Type        string `json:"gameType"`
-	CreaterEmail string `json:"createrEmail"`
+	Id           int        `json:"id"`
+	Name         string     `json:"gameName"`
+	Type         string     `json:"gameType"`
+	CreaterEmail string     `json:"createrEmail"`
+	CreatedAt    *time.Time `json:"created_at,omitempty"`
+	UpdatedAt    *time.Time `json:"updated_at,omitempty"`
 }
 
 type Score struct {
-	Round int `json:"round"`
-	Point int `json:"point"`
-	PlayerName string `json:"playerName"`
-	Throw int `json:"throw"`
+	Id         int        `json:"id"`
+	Round      int        `json:"round"`
+	Point      int        `json:"point"`
+	PlayerName string     `json:"playerName"`
+	Throw      int        `json:"throw"`
+	CreatedAt  *time.Time `json:"created_at,omitempty"`
+	UpdatedAt  *time.Time `json:"updated_at,omitempty"`
 }
 
 type Player struct {
-	UserId int `json:"userId"`
+	UserId    int        `json:"userId"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 }
