ee8a0a5182b35d0f60d0ba4d6073ef96801cb21a
jeel <zeelrupapara@gmail.com>
2022-03-31 17:44:19 +0530

    fix: update game struct craeterid datatype

--- api/models/user.go
+++ api/models/user.go
@@ -16,7 +16,7 @@ type Game struct {
 	Id            int        `json:"id"`
 	Name          string     `json:"gameName"`
 	Type          string     `json:"gameType"`
-	CreaterUserId string     `json:"createrUserId"`
+	CreaterUserId int     `json:"createrUserId"`
 	CreatedAt     *time.Time `json:"created_at,omitempty"`
 	UpdatedAt     *time.Time `json:"updated_at,omitempty"`
 }
