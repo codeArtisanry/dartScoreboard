415f64b3c6ec6ab23a43ed1410392968a4ddc078
Vatsal <chauhanvatsal55@gmail.com>
2022-04-04 12:54:31 +0530

    fix: middleware

--- api/controllers/authControllers.go
+++ api/controllers/authControllers.go
@@ -9,19 +9,41 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	gf "github.com/shareed2k/goth_fiber"
 )
 
-type Cookie struct {
-	Name     string
-	Value    string
-	Path     string
-	Domain   string
-	Expires  time.Time
-	Secure   bool
-	HTTPOnly bool
-	SameSite string // lax, strict, none
+// 1. Endpoint for i am logged in?
+func Endpoint(ctx *fiber.Ctx) error {
+	cookie := ctx.Cookies("user")
+	fmt.Println("this is cookie", cookie)
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		pem, err := getGooglePublicKey(fmt.Sprintf("%s", token.Header["kid"]))
+		if err != nil {
+			return nil, err
+		}
+		key, err := jwt.ParseRSAPublicKeyFromPEM([]byte(pem))
+		if err != nil {
+			return nil, err
+		}
+		return key, nil
+	})
+	claims := token.Claims.(*jwt.StandardClaims)
+	fmt.Println("claims :", claims)
+	if err != nil {
+		fmt.Println("err  :", err)
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "User NOT Exist",
+		})
+	} else {
+		return ctx.Status(fiber.StatusAccepted).JSON(fiber.Map{
+			"message": "User Exist",
+		})
+	}
+	// c.Redirect("/auth/google")
+	// If exist then return 2xx status code
+	// Else return 403 unauthorized
 }
 
 
@@ -41,6 +63,7 @@ func GoogleRedirect(ctx *fiber.Ctx) error {
 	if err != nil {
 		return err
 	}
+
 	// fmt.Println(user)
 	userinfo := models.User{
 		// Id:      user.UserID,
@@ -48,9 +71,7 @@ func GoogleRedirect(ctx *fiber.Ctx) error {
 		// Picture: user.AvatarURL,
 	}
 	// db := models.Database()
-	fmt.Println("from api", userinfo)
 	// models.InsertData(db, userinfo)
-
 	// GET TOKEN &
 	// TODO: Set cookie
 	cookie := new(fiber.Cookie)
@@ -61,7 +82,7 @@ func GoogleRedirect(ctx *fiber.Ctx) error {
 	// Set cookie from JWT
 	ctx.Cookie(cookie)
 	// TODO: Redirect user to frontend
-	ctx.Redirect("/home")
+	// ctx.Redirect("/home/:id")
 	return ctx.JSON(fiber.Map{
 		"message": "success",
 		"data":    userinfo,
