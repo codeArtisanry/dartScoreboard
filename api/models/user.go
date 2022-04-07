71f40bcd16068ffd41deac85679c9d08483c4ac7
jeel <zeelrupapara@gmail.com>
2022-04-07 08:56:34 +0530

    feat: add omitempty in game struct createruserid field

--- api/models/user.go
+++ api/models/user.go
@@ -7,7 +7,7 @@ type User struct {
 	FirstName string     `json:"firstName"`
 	LastName  string     `json:"lastName"`
 	Email     string     `json:"email"`
-	AvatarURL string     `json:"avatarUrl"`
+	AvatarURL string     `json:"avatarUrl,omitempty"`
 	CreatedAt *time.Time `json:"created_at,omitempty"`
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 }
