cb7748ad17eb26ce8c87f3d8207e4e2f249339c7
payal <payal@improwised.com>
2022-04-20 10:58:38 +0530

    feat: add swagger

--- api/controllers/activeStatusRes.go
+++ api/controllers/activeStatusRes.go
@@ -4,10 +4,19 @@ import (
 	models "dartscoreboard/models/database"
 	types "dartscoreboard/models/types"
 	"fmt"
-	"github.com/gofiber/fiber/v2"
 	"strconv"
+
+	"github.com/gofiber/fiber/v2"
 )
 
+// swagger:route GET/games/{id}/active-status ActiveStatus activeStatus
+// Get activestatus using game id
+// Responses:
+//  200: ActiveStatus
+//  400: StatusCode
+//  404: StatusCode
+//  500: StatusCode
+// GetActiveStatus are get that res which is you want to fetch
 func GetActiveStatusRes(ctx *fiber.Ctx) error {
 	id := ctx.Params("id")
 	gameId, err := strconv.Atoi(id)
