d1c3fdf317380dfcf2823ba3cb5fa35e0ad8d983
Vatsal <87218847+codesnail21@users.noreply.github.com>
2022-04-26 16:17:29 +0530

    fix: TCP error (#58)
    * fix: TCP error
    
    * feat: add requested changes
    
    * add env.example
    
    * fix: set up to my local values

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -74,7 +74,11 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: `${process.env.BACKEND_URL}/`,
+    proxy: true,
+  },
+  proxy: {
+    '/api': `${process.env.BACKEND_URL}/`,
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
