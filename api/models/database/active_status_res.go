e199508c313f1cd3f28515b81c8b28fd8bdf4be3
zeelrupapara <zeelrupapara@gmail.com>
2022-04-26 11:48:39 +0530

    fix: change naming conv

--- api/models/database/active_status_res.go
+++ api/models/database/active_status_res.go
@@ -11,29 +11,29 @@ import (
 // GetActiveStatusRes from from Games table By Game Id
 func GetActiveStatusRes(db *sql.DB, id int, activeRes types.ActiveStatus) (types.ActiveStatus, error) {
 	var (
-		activeResJson types.ActiveStatus
-		playersIds    []int
-		count         int
-		typeOfGame    string
-		status        string
-		playerId      int
+		activeResJson    types.ActiveStatus
+		playersIds       []int
+		numOfRowsPerGame int
+		typeOfGame       string
+		status           string
+		playerId         int
 	)
-	queryofcount := fmt.Sprintf("SELECT COUNT(scores.id) from scores inner join game_players on game_players.id=scores.game_player_id WHERE game_players.game_id = %d;", id)
-	row := db.QueryRow(queryofcount)
-	err := row.Scan(&count)
+	findNumOfRowsPerGame := fmt.Sprintf("SELECT COUNT(scores.id) from scores inner join game_players on game_players.id=scores.game_player_id WHERE game_players.game_id = %d;", id)
+	row := db.QueryRow(findNumOfRowsPerGame)
+	err := row.Scan(&numOfRowsPerGame)
 	if err != nil {
 		fmt.Println(err)
 		return activeResJson, err
 	}
-	queryoftype := fmt.Sprintf("SELECT type, status from games WHERE id=%d", id)
-	rowoftype := db.QueryRow(queryoftype)
+	findGameInfo := fmt.Sprintf("SELECT type, status from games WHERE id=%d", id)
+	rowoftype := db.QueryRow(findGameInfo)
 	err = rowoftype.Scan(&typeOfGame, &status)
 	if err != nil {
 		fmt.Println()
 		return activeResJson, err
 	}
-	queryofuserid := fmt.Sprintf("SELECT user_id from game_players WHERE game_id = %d", id)
-	rows, err := db.Query(queryofuserid)
+	findPlayersIdsPerGame := fmt.Sprintf("SELECT user_id from game_players WHERE game_id = %d", id)
+	rows, err := db.Query(findPlayersIdsPerGame)
 	if err != nil {
 		fmt.Println(err)
 		return activeResJson, err
@@ -46,19 +46,17 @@ func GetActiveStatusRes(db *sql.DB, id int, activeRes types.ActiveStatus) (types
 		}
 		playersIds = append(playersIds, playerId)
 	}
-	maping := make(map[int]int)
-	for i := 0; i < len(playersIds); i++ {
-		maping[playersIds[i]] = i
-		fmt.Println("ggg", maping[playersIds[i]])
+	mapPlayersIds := make(map[int]int)
+	for playerIdIndex := 0; playerIdIndex < len(playersIds); playerIdIndex++ {
+		mapPlayersIds[playersIds[playerIdIndex]] = playerIdIndex
 	}
-	//counter := 0
-	if count == 0 {
-		insert, err := db.Prepare("UPDATE games set status='In Progress' WHERE id = ?")
+	if numOfRowsPerGame == 0 {
+		update, err := db.Prepare("UPDATE games set status='In Progress' WHERE id = ?")
 		if err != nil {
 			fmt.Println(err)
 			return activeResJson, err
 		}
-		_, err = insert.Exec(id)
+		_, err = update.Exec(id)
 		if err != nil {
 			fmt.Println(err)
 			return activeResJson, err
@@ -66,39 +64,36 @@ func GetActiveStatusRes(db *sql.DB, id int, activeRes types.ActiveStatus) (types
 		activeRes.Round = 1
 		activeRes.Throw = 1
 		activeRes.PlayerId = playersIds[0]
-
 	} else {
-		query := fmt.Sprintf("SELECT rounds.round, game_players.user_id as player_id, scores.throw FROM scores INNER JOIN rounds ON scores.round_id = rounds.id INNER JOIN game_players ON scores.game_player_id = game_players.id WHERE rounds.game_id = %d ORDER BY scores.id DESC LIMIT 1 ;", id)
-		row = db.QueryRow(query)
+		findCurrentTurnInfo := fmt.Sprintf("SELECT rounds.round, game_players.user_id as player_id, scores.throw FROM scores INNER JOIN rounds ON scores.round_id = rounds.id INNER JOIN game_players ON scores.game_player_id = game_players.id WHERE rounds.game_id = %d ORDER BY scores.id DESC LIMIT 1 ;", id)
+		row = db.QueryRow(findCurrentTurnInfo)
 		err = row.Scan(&activeRes.Round, &activeRes.PlayerId, &activeRes.Throw)
 		if err != nil {
 			fmt.Println(err)
 			return activeResJson, err
 		}
-		fmt.Println("esr", count)
-		if typeOfGame == "High Score" && count%(9*len(playersIds)) == 0 {
+		if typeOfGame == "High Score" && numOfRowsPerGame%(9*len(playersIds)) == 0 {
 			activeRes.Round = 0
 			activeRes.PlayerId = 0
 			activeRes.Throw = 0
 
-			insert, err := db.Prepare("UPDATE games SET status = 'Completed' WHERE id = ?")
+			update, err := db.Prepare("UPDATE games SET status = 'Completed' WHERE id = ?")
 			if err != nil {
 				fmt.Println(err)
 				return activeResJson, err
 			}
-			_, err = insert.Exec(id)
+			_, err = update.Exec(id)
 			if err != nil {
 				fmt.Println(err)
 				return activeResJson, err
 			}
 		} else {
-
-			if count%(3*len(playersIds)) == 0 {
+			if numOfRowsPerGame%(3*len(playersIds)) == 0 {
 				activeRes.Round = activeRes.Round + 1
 			}
 			if activeRes.Throw%3 == 0 {
 				activeRes.Throw = 1
-				test := maping[activeRes.PlayerId]
+				test := mapPlayersIds[activeRes.PlayerId]
 				if test == (len(playersIds) - 1) {
 					activeRes.PlayerId = playersIds[0]
 				} else {
