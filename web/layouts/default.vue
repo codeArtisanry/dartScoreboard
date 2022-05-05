94fdee5351eb9bc90ecf60bec015e31be747b783
Vatsal <87218847+codesnail21@users.noreply.github.com>
2022-05-05 16:02:52 +0530

    feat: add loading spinner (#92)

---
+++ web/layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <LoadingSpinner v-if="showHideSpinner" />
+
+    <Nuxt />
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      showHideSpinner: true,
+    }
+  },
+  beforeCreate() {
+    this.showHideSpinner = true
+  },
+  mounted() {
+    this.showHideSpinner = false
+  },
+}
+</script>
