73b891e4282c51c10781d917a744be21512cd188
zeelrupapara <zeelrupapara@gmail.com>
2022-05-18 17:55:10 +0530

    fix: get err when turn is already deleted in undo api

--- api/controllers/undoScore.go
+++ api/controllers/undoScore.go
@@ -16,7 +16,28 @@ func UndoScore(ctx *fiber.Ctx) error {
 			Message:    "Bad Request",
 		})
 	}
-	err = models.UndoScore(db, gameId)
+	round, err := strconv.Atoi(ctx.Params("roundid"))
+	if err != nil {
+		return ctx.Status(400).JSON(types.StatusCode{
+			StatusCode: 400,
+			Message:    "Bad Request",
+		})
+	}
+	playerId, err := strconv.Atoi(ctx.Params("playerid"))
+	if err != nil {
+		return ctx.Status(400).JSON(types.StatusCode{
+			StatusCode: 400,
+			Message:    "Bad Request",
+		})
+	}
+	turn, err := strconv.Atoi(ctx.Params("turnid"))
+	if err != nil {
+		return ctx.Status(400).JSON(types.StatusCode{
+			StatusCode: 400,
+			Message:    "Bad Request",
+		})
+	}
+	err = models.UndoScore(db, gameId, round, playerId, turn)
 	if err != nil {
 		return ctx.Status(500).JSON(types.StatusCode{
 			StatusCode: 500,
--- api/models/database/undo_score.go
+++ api/models/database/undo_score.go
@@ -13,38 +13,65 @@ type lastScoreDetails struct {
 	score    int
 	validate string
 }
+type PreviousTurn struct {
+	round    int
+	playerId int
+	turn     int
+}
 
-// Delete Last Inserted Score By GameId
-func UndoScore(db *sql.DB, id int) error {
-	for i := 1; i <= 3; i++ {
-		lastScoreDetails, err := FindLastScoreId(db, id, lastScoreDetails{})
+// Find players of particular game by gameId
+func FindGamePlayers(db *sql.DB, gameId int) ([]int, error) {
+	var playersList []int
+	var player int
+	query := fmt.Sprintf("SELECT user_id FROM game_players WHERE game_id = %d;", gameId)
+	rows, err := db.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return playersList, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err = rows.Scan(&player)
 		if err != nil {
-			log.Println(err)
-			return err
+			fmt.Println(err)
+			return playersList, err
 		}
-		if lastScoreDetails.validate == "INVALID" && lastScoreDetails.score == 0 {
-			err = DeleteScore(db, lastScoreDetails.scoreId)
-			if err != nil {
-				log.Println(err)
-				return err
-			}
-			log.Println("Successfully Deleted", lastScoreDetails.scoreId, "ScoreId")
+		playersList = append(playersList, player)
+	}
+	return playersList, nil
+}
+
+// Find Previous turn, playerId and round
+func FindPreviousTurn(round int, playerId int, turn int, playersId []int) PreviousTurn {
+	var currentPlayerIndex int
+	mapPlayerid := make(map[int]int)
+	for i := 0; i <= len(playersId)-1; i++ {
+		mapPlayerid[playersId[i]] = i
+	}
+	turn = turn - 1
+	if turn == 0 {
+		turn = 3
+		currentPlayerIndex = mapPlayerid[playerId]
+		currentPlayerIndex = currentPlayerIndex - 1
+		if currentPlayerIndex == -1 {
+			currentPlayerIndex = len(playersId) - 1
+			playerId = playersId[currentPlayerIndex]
+			round = round - 1
 		} else {
-			err = DeleteScore(db, lastScoreDetails.scoreId)
-			if err != nil {
-				log.Println(err)
-				return err
-			}
-			log.Println("Successfully Deleted", lastScoreDetails.scoreId, "ScoreId")
-			break
+			playerId = playersId[currentPlayerIndex]
 		}
 	}
-	return nil
+	previousTurn := PreviousTurn{
+		round:    round,
+		playerId: playerId,
+		turn:     turn,
+	}
+	return previousTurn
 }
 
 // Find Last Inserted Scoreid
-func FindLastScoreId(db *sql.DB, id int, lastScoreDetails lastScoreDetails) (lastScoreDetails, error) {
-	findLastScoreId := fmt.Sprintf("SELECT id,score,is_valid FROM scores WHERE game_player_id = (SELECT id FROM game_players WHERE game_id = %d) ORDER BY id DESC LIMIT 1", id)
+func FindLastScoreId(db *sql.DB, gameId int, lastScoreDetails lastScoreDetails, previosTurn PreviousTurn) (lastScoreDetails, error) {
+	findLastScoreId := fmt.Sprintf("SELECT id,score,is_valid FROM scores WHERE game_player_id = (SELECT id FROM game_players WHERE game_id = %d AND user_id = %d) AND round_id = (SELECT id FROM rounds WHERE round = %d AND game_id = %d) AND throw = %d", gameId, previosTurn.playerId, previosTurn.round, gameId, previosTurn.turn)
 	lastScoreId := db.QueryRow(findLastScoreId)
 	err := lastScoreId.Scan(&lastScoreDetails.scoreId, &lastScoreDetails.score, &lastScoreDetails.validate)
 	if err != nil {
@@ -65,6 +92,41 @@ func DeleteScore(db *sql.DB, scoreId int) error {
 	if err != nil {
 		log.Println(err)
 		return err
+	}
+	return nil
+}
+
+// Delete Last Inserted Score By GameId for undo action
+func UndoScore(db *sql.DB, gameId int, round int, playerId int, turn int) error {
+	gamePlayerList, err := FindGamePlayers(db, gameId)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	previousTurn := FindPreviousTurn(round, playerId, turn, gamePlayerList)
+	for i := 1; i <= 3; i++ {
+		lastScoreDetails, err := FindLastScoreId(db, gameId, lastScoreDetails{}, previousTurn)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		if lastScoreDetails.validate == "INVALID" && lastScoreDetails.score == 0 {
+			err = DeleteScore(db, lastScoreDetails.scoreId)
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+			log.Println("Successfully Deleted", lastScoreDetails.scoreId, "ScoreId")
+		} else {
+			err = DeleteScore(db, lastScoreDetails.scoreId)
+			if err != nil {
+				log.Println(err)
+				return err
+			}
+			log.Println("Successfully Deleted", lastScoreDetails.scoreId, "ScoreId")
+			break
+		}
+		previousTurn = FindPreviousTurn(previousTurn.round, previousTurn.playerId, previousTurn.turn, gamePlayerList)
 	}
 	return nil
 }
