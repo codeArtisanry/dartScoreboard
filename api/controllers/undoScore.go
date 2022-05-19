798d22118f9847356ff88332b2f4d2fdd7c22152
zeelrupapara <zeelrupapara@gmail.com>
2022-05-19 14:24:06 +0530

    fix: validate your turn with current turn

--- api/controllers/undoScore.go
+++ api/controllers/undoScore.go
@@ -37,12 +37,27 @@ func UndoScore(ctx *fiber.Ctx) error {
 			Message:    "Bad Request",
 		})
 	}
-	err = models.UndoScore(db, gameId, round, playerId, turn)
+	activeRes := types.ActiveStatus{}
+	currentStateOfGame, err := models.GetActiveStatusRes(db, gameId, activeRes)
 	if err != nil {
-		return ctx.Status(500).JSON(types.StatusCode{
+		return ctx.Status(400).JSON(types.StatusCode{
 			StatusCode: 500,
 			Message:    "Internal Server Error",
 		})
 	}
-	return ctx.SendStatus(204)
+	if playerId == currentStateOfGame.PlayerId && round == currentStateOfGame.Round && turn == currentStateOfGame.Throw {
+		err = models.UndoScore(db, gameId, round, playerId, turn)
+		if err != nil {
+			return ctx.Status(500).JSON(types.StatusCode{
+				StatusCode: 500,
+				Message:    "Internal Server Error",
+			})
+		}
+		return ctx.SendStatus(204)
+	} else {
+		return ctx.Status(400).JSON(types.StatusCode{
+			StatusCode: 400,
+			Message:    "Your Turn is not Matching With Current Turn",
+		})
+	}
 }
